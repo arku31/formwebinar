e6efbb4051c3bc07b08bdc004fbbe5a11ac34f5a
Igor Tverdokhleb <itvrd2@yandex.ru>
2017-06-28 16:10:09 +0500

    Delete 1.php

--- 1.php
+++
@@ -1,5 +0,0 @@
-<?php
-$mail->Host = 'smtp.mail.ru';  // Specify main and backup SMTP servers
-$mail->Username = 'fsdfsdfsdf.05@bk.ru';                 // SMTP username
-$mail->Password = 'yjdbxrb#123';                           // SMTP password
-$mail->setFrom('fsdfsdfsdf.05@bk.ru', 'mr. fsd');
